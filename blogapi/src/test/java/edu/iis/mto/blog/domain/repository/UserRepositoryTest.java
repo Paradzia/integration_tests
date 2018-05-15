962bc94d558e174be5e9245fba6fbb7e75895c19
unknown <paradowskim94@gmail.com>
2018-05-15 17:15:12 +0200

    added user finder tests and some fixes with ignored tests

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.blog.domain.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hamcrest.Matchers;
@@ -11,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Example;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.iis.mto.blog.domain.model.AccountStatus;
@@ -32,37 +34,70 @@ public class UserRepositoryTest {
     public void setUp() {
         user = new User();
         user.setFirstName("Jan");
-        user.setEmail("john@domain.com");
+        user.setEmail("johnAlpha@domain.com");
         user.setAccountStatus(AccountStatus.NEW);
     }
 
     @Test
     public void shouldFindNoUsersIfRepositoryIsEmpty() {
-        //repository.deleteAll();
         List<User> users = repository.findAll();
                 //Przed uruchomieniem testu tworzymy nowego uzytkownika, wiec albo na poczatku czyscimy cala liste,
                 // albo uwzgledniamy go i nowe repozytorium powinno miec wtedy jednego, a nie zero userow.
+                //2018-05-15 16:49:18.741  INFO 10788 --- [           main] org.hibernate.tool.hbm2ddl.SchemaExport  : HHH000476: Executing import script '/import.sql'
+       // System.out.println(users.get(0).getEmail());
         Assert.assertThat(users, Matchers.hasSize(1));
         //Assert.assertThat(users, Matchers.hasSize(0));
     }
 
-    @Ignore
     @Test
     public void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
         User persistedUser = entityManager.persist(user);
         List<User> users = repository.findAll();
 
-        Assert.assertThat(users, Matchers.hasSize(1));
-        Assert.assertThat(users.get(0).getEmail(), Matchers.equalTo(persistedUser.getEmail()));
+        Assert.assertThat(users, Matchers.hasSize(2));
+        Assert.assertThat(users.get(1).getEmail(), Matchers.equalTo(persistedUser.getEmail()));
     }
 
-    @Ignore
+
     @Test
     public void shouldStoreANewUser() {
 
         User persistedUser = repository.save(user);
 
         Assert.assertThat(persistedUser.getId(), Matchers.notNullValue());
+    }
+
+    @Test
+    public void shouldFindTwoUsers(){
+        User jan = new User();
+        jan.setFirstName("Jan");
+        jan.setEmail("JanAlpha@domain.com");
+        jan.setAccountStatus(AccountStatus.NEW);
+
+        entityManager.persist(user);
+        entityManager.persist(jan);
+        List<User> users = repository.findAll();
+        List<User> testers = new ArrayList<>();
+
+        for(User user : users){
+            if(user.getFirstName().equalsIgnoreCase("jan")){
+                testers.add(user);
+            }
+        }
+        Assert.assertThat(testers.size(),Matchers.is(2));
+    }
+
+    @Test
+    public void shouldNotFindAnyUser(){
+        entityManager.persist(user);
+        List<User> users = repository.findAll();
+        List<User> testers = new ArrayList<>();
+        for(User user : users){
+            if(user.getEmail().equalsIgnoreCase("testowyuser01@domena.ork")){
+                testers.add(user);
+            }
+        }
+        Assert.assertThat(testers.size(),Matchers.is(0));
     }
 
 }
