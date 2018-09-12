9a8a829be669d528c8b5bda5b464eed262fc543d
Parada <paradowskim94@gmail.com>
2018-09-12 19:33:00 +0200

    3.1 fixed user repository bug - deleted unnecessary sql import

--- blogapi/src/test/resources/import.sql
+++ blogapi/src/test/resources/import.sql
@@ -1,2 +1,1 @@
---this script initiates db for integration tests 
-insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward') 
+--this script initiates db for integration tests
