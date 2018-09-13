7363ffae64fa91a1732492113dd2fc0a39618c6a
Parada <paradowskim94@gmail.com>
2018-09-13 18:52:55 +0200

    3.2 only confirmed user can like posts now.

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,7 +1,12 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -31,6 +36,24 @@ public class BlogManagerTest {
     @Autowired
     BlogService blogService;
 
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
+    @MockBean
+    LikePostRepository likePostRepository;
+
+    private User newUser;
+    private User owner;
+    @Before
+    public void setUp() throws Exception {
+        owner = new User();
+        owner.setId(1L);
+
+        newUser = new User();
+        newUser.setAccountStatus(AccountStatus.NEW);
+        newUser.setId(2L);
+    }
+
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
         blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
@@ -40,4 +63,17 @@ public class BlogManagerTest {
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
 
+    @Test(expected = DomainError.class)
+    public void likingPostByNotConfirmedUserShouldThrowException(){
+
+        BlogPost blogPost = new BlogPost();
+        blogPost.setId(1L);
+        blogPost.setUser(owner);
+
+        Mockito.when(userRepository.findOne(1L)).thenReturn(owner);
+        Mockito.when(userRepository.findOne(2L)).thenReturn(newUser);
+        Mockito.when(blogPostRepository.findOne(1L)).thenReturn(blogPost);
+
+        blogService.addLikeToPost(newUser.getId(), blogPost.getId());
+    }
 }
